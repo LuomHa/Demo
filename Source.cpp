0e225b247783b9280ada83f774c81f3242d8cd36
caouyen <nguyencaouyen99pk@gmail.com>
2020-10-29 10:51:37 +0700

    tinh thuong

--- Source.cpp
+++ Source.cpp
@@ -14,12 +14,33 @@ int main()
 	cin >> b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT\n";
 	cout << "Hieu 2 số a-b= "<< hieu(a,b);
-	system("pause");
-	return 0;
+	cout<< " Thuong 2 số a/b="<<thuong(a,b);
+	cout << "Tong 2 số a+b= "<< tong(a,b);
+	cout << "Tich 2 số a+b= "<< tich(a,b);
+	
+	
 }
 int hieu(int a, int b)
 {
 	int s = 0;
 	s = a - b;
+	return s;
+}
+int thuong(int a, int b)
+{
+	int s = 0;
+	s = a / b;
+	return s;
+}
+int tong(int a, int b)
+{
+	int s = 0;
+	s = a + b;
+	return s;
+}
+int tich(int a, int b)
+{
+	int s = 0;
+	s = a * b;
 	return s;
 }
