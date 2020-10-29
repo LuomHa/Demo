bf06f779020e67578380691e577d534afdab92ef
vankha <luongvankha02021999@gmail.com>
2020-10-29 11:28:35 +0700

    chu vi dien tich hinh vuong , chu nhat

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,9 @@ int tich(int a, int b);
 float thuong(int a, int b);
 float tinhchuvihcn(int cd, int cr);
 float dientichhcn(int cd, int cr);
+int tinhdientichhinhvuong(int a);
+int tinhchivihinhvuong(int a);
+	
 int main()
 {
 	int a, b;
@@ -15,21 +18,16 @@ int main()
 	cout << "Mời bạn nhập b: ";
 	cin >> b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT\n";
-<<<<<<< HEAD
 	cout << "Hieu 2 số a-b= "<< hieu(a,b);
 	cout<< " Thuong 2 số a/b="<<thuong(a,b);
 	cout << "Tong 2 số a+b= "<< tong(a,b);
 	cout << "Tich 2 số a+b= "<< tich(a,b);
-	
-	
-=======
-	cout << "Tong 2 số a+b= "<< tong(a,b);
-	cout << "hieu 2 số a-b= "<< hieu(a,b);
 	cout << "chu vi hinh chu nhat =" << tinhchuvihcn(a, b);
 	cout << "dien tich hinh chu nhat=" << dientichhcn(a, b);
+	cout << "chu vi hinh vuong canh a " << tinhchivihinhvuong(a);
+	cout << "dien tich hinh vuong canh a " << tinhdientichhinhvuong(a);
 	system("pause");
 	return 0;
->>>>>>> fdc6e696ce18c202209cad49b0621278f9af6ed1
 }
 int tong(int a, int b)
 {
@@ -43,26 +41,21 @@ int hieu(int a, int b)
 	s = a - b;
 	return s;
 }
-<<<<<<< HEAD
+
 int thuong(int a, int b)
 {
 	int s = 0;
 	s = a / b;
 	return s;
 }
-int tong(int a, int b)
-{
-	int s = 0;
-	s = a + b;
-	return s;
-}
+
 int tich(int a, int b)
 {
 	int s = 0;
 	s = a * b;
 	return s;
 }
-=======
+
 float tinhchuvihcn(int cd, int cr)
 {
 	float cv = (cd + cr) * 2;
@@ -73,4 +66,15 @@ float dientichhcn(int cd, int cr)
 	float dt = cd * cr;
 	return dt;
 }
->>>>>>> fdc6e696ce18c202209cad49b0621278f9af6ed1
+int tinhchivihinhvuong(int a){
+	int s = 0;
+	S=a*2;
+	return s;
+
+}
+int tinhdientichhinhvuong(int a){
+	int s = 0;
+	S=a*a;
+	return s;
+
+}
